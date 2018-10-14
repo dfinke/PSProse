490d4df4b987bebff562a3513f61a23dee928755
dfinke <finked@hotmail.com>
2018-10-14 12:41:30 -0400

    Added examples and psm1

---
+++ ExtractJson.psm1
@@ -0,0 +1,22 @@
+Get-ChildItem $PSScriptRoot\lib | ForEach-Object {Add-Type -Path $_.FullName}
+
+function New-JSONSession {
+    New-Object Microsoft.ProgramSynthesis.Extraction.Json.Session $null, $null, $null
+}
+
+function Get-JSONData {
+    param(
+        $jsonFile
+    )
+
+    $jsonText = Get-Content $jsonFile -Raw
+
+    $noJoinSession = New-JSONSession
+    $noJoinSession.Inputs.Add($jsonText)
+    $noJoinProgram = $noJoinSession.Learn()
+    $table = $noJoinProgram.Run($jsonText)
+    # $noJoinProgram.Run($jsonText)
+
+
+    foreach ($row in $table) {$row -join ', '}
+}
---
+++ examples/Get-People.ps1
@@ -0,0 +1,3 @@
+Import-Module ..\ExtractJSON.psm1 -Force
+
+Get-JSONData .\people.json
