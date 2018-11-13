ee63a7cd2ab9866e1060595a72c00f1aafbea637
dfinke <finked@hotmail.com>
2018-11-13 12:42:57 -0500

    Added split text tests

---
+++ __tests__/SplitText.tests.ps1
@@ -0,0 +1,53 @@
+﻿Import-Module $PSScriptRoot\..\PSProse.psd1 -Force
+
+Describe "Test Invoke-SplitText" {
+    BeforeEach {
+        $file = "$PSScriptRoot\splitDataSample1.txt"
+    }
+
+    It "Should return three records with four columns" {
+        $actual = Invoke-SplitText -file $file
+
+        $actual.Count | Should Be 3
+        $actual[0].psobject.properties.name.count | Should Be 4
+    }
+
+    It "Should have numbered column names" {
+        $actual = Invoke-SplitText -file $file
+        
+        $propertyNames = $actual[0].psobject.properties.name
+        $propertyNames[0] | Should Be "Col1"
+        $propertyNames[1] | Should Be "Col2"
+        $propertyNames[2] | Should Be "Col3"
+        $propertyNames[3] | Should Be "Col4"
+    }
+
+    It "Propertynames should be the headers passed in" {
+    
+        $actual = Invoke-SplitText -file $file -Header 'ID','First','Last','Country'
+        
+        $propertyNames = $actual[0].psobject.properties.name
+        
+        $propertyNames[0] | Should Be "ID"        
+        $propertyNames[1] | Should Be "First"
+        $propertyNames[2] | Should Be "Last"
+        $propertyNames[3] | Should Be "Country"
+    }
+
+    It "Single constraint should yield different results" {
+        $param=@{
+            file = $file 
+            Header = 'ID','First','Last','Country'
+            Constraints = $(
+                'pe5', 'leonard', 'robledo', 'australia'
+            )
+        }
+
+        $actual = Invoke-SplitText @param
+
+        $actual[0].ID    | Should Be "pe5"
+        $actual[0].First | Should Be "leonard"
+        $actual[0].Last | Should Be "robledo"
+        $actual[0].Country | Should Be "australia"
+    }
+}
