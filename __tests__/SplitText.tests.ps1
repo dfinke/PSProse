d52ccdd92904b061c89ab508828a8cc61929dd85
dfinke <finked@hotmail.com>
2018-11-13 13:43:20 -0500

    Add tests for multiple constraints

--- __tests__/SplitText.tests.ps1
+++ __tests__/SplitText.tests.ps1
@@ -14,7 +14,7 @@ Describe "Test Invoke-SplitText" {
 
     It "Should have numbered column names" {
         $actual = Invoke-SplitText -file $file
-        
+
         $propertyNames = $actual[0].psobject.properties.name
         $propertyNames[0] | Should Be "Col1"
         $propertyNames[1] | Should Be "Col2"
@@ -23,31 +23,58 @@ Describe "Test Invoke-SplitText" {
     }
 
     It "Propertynames should be the headers passed in" {
-    
-        $actual = Invoke-SplitText -file $file -Header 'ID','First','Last','Country'
-        
+
+        $actual = Invoke-SplitText -file $file -Header 'ID', 'First', 'Last', 'Country'
+
         $propertyNames = $actual[0].psobject.properties.name
-        
-        $propertyNames[0] | Should Be "ID"        
+
+        $propertyNames[0] | Should Be "ID"
         $propertyNames[1] | Should Be "First"
         $propertyNames[2] | Should Be "Last"
         $propertyNames[3] | Should Be "Country"
     }
 
     It "Single constraint should yield different results" {
-        $param=@{
-            file = $file 
-            Header = 'ID','First','Last','Country'
-            Constraints = $(
-                'pe5', 'leonard', 'robledo', 'australia'
-            )
+        $param = @{
+            file        = $file
+            Header      = 'ID', 'First', 'Last', 'Country'
+            Constraints = [ordered]@{
+                0 = 'pe5', 'leonard', 'robledo', 'australia'
+            }
         }
 
         $actual = Invoke-SplitText @param
+
+        $actual.Count | Should Be 3
 
         $actual[0].ID    | Should Be "pe5"
         $actual[0].First | Should Be "leonard"
         $actual[0].Last | Should Be "robledo"
         $actual[0].Country | Should Be "australia"
+    }
+
+    It "Multiple constraints should yield different results" {
+        $param = @{
+            file        = $file
+            Header      = 'ID', 'First', 'Last', 'Country'
+            Constraints = [ordered]@{
+                0 = echo pe5 leonard robledo australia
+                1 = echo u109 adam 'jay lucas' 'new zealand'
+            }
+        }
+
+        $actual = Invoke-SplitText @param
+
+        $actual.Count | Should Be 3
+
+        $actual[0].ID    | Should Be "pe5"
+        $actual[0].First | Should Be "leonard"
+        $actual[0].Last | Should Be "robledo"
+        $actual[0].Country | Should Be "australia"
+
+        $actual[1].ID    | Should Be "u109"
+        $actual[1].First | Should Be "adam"
+        $actual[1].Last | Should Be "jay lucas"
+        $actual[1].Country | Should Be "new zealand"
     }
 }
