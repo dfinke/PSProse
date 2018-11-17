72d60908dd2031034b42dff70664bcfdd47e3100
dfinke <finked@hotmail.com>
2018-11-17 11:15:00 -0500

    Added simple example

---
+++ examples/simpleDataSimpleExample.ps1
@@ -0,0 +1,5 @@
+##
+# Specify a target file
+# Add an "example" of what the data should look like when split with `Contraints`
+##
+Invoke-SplitText -File splitDataSample1.txt -Constraints @("pe5", 'leonard', 'robledo', 'australia')
